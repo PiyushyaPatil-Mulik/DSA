c58a725967e244d80c422ee3fafd0283ea8de979
PiyushyaPatil-Mulik <piyushamulik566@gmail.com>
2025-10-09 12:50:12 +0530

    added find smallest ele in given array.

---
+++ basicArray/smallestEle.java
@@ -0,0 +1,25 @@
+package DSA.basicArray;
+import javax.swing.*;
+
+public class smallestEle {
+    public static void main(String[] args) {
+     String a=JOptionPane.showInputDialog("Enter Array Size");
+    int n=Integer.parseInt(a);
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        String ele=JOptionPane.showInputDialog("Enter Array ele" + (i+1));
+        arr[i]=Integer.parseInt(ele);
+    }
+System.out.println("Entered Array is");
+    for(int j=0;j<n;j++){
+        System.out.println(arr[j]);
+    }
+    int min=arr[0];
+    for(int k=1;k<n;k++){
+        if(arr[k]<min){
+            min=arr[k];
+        }
+    }
+    System.out.println("Smallest Ele is "+min);
+}
+}
